b5612553dc25d121d689029181d938e9ef7356ae
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-08 19:10:31 -0300

    teste 1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+  res.send('Application is running')
 });
 
 app.listen(8081, ()=>{ console.log('Server is running on port 8081'); });
